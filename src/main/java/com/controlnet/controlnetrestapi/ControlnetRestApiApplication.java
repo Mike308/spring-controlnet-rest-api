55b19af87c7397c5f0421cd4882c854a58a5b2c2
Mikołaj Werner <m.werner9205@gmail.com>
2018-11-29 19:49:55 +0100

    - init commit

---
+++ src/main/java/com/controlnet/controlnetrestapi/ControlnetRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.controlnet.controlnetrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlnetRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlnetRestApiApplication.class, args);
+    }
+}
