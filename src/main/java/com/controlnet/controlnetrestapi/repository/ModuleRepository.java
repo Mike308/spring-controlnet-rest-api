a66277fc0ac9846acf868348c4dd79865178f21b
Mikołaj Werner <m.werner9205@gmail.com>
2018-11-30 19:18:53 +0100

    - added module repository

---
+++ src/main/java/com/controlnet/controlnetrestapi/repository/ModuleRepository.java
@@ -0,0 +1,11 @@
+package com.controlnet.controlnetrestapi.repository;
+
+import com.controlnet.controlnetrestapi.model.Module;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModuleRepository extends CrudRepository<Module, Integer> {
+
+}
+
