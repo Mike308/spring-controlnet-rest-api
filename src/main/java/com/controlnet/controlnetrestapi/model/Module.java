7a7281a2846350816490b385c6170648699c392f
Mikołaj Werner <m.werner9205@gmail.com>
2018-11-30 19:16:46 +0100

    - added Module model

---
+++ src/main/java/com/controlnet/controlnetrestapi/model/Module.java
@@ -0,0 +1,55 @@
+package com.controlnet.controlnetrestapi.model;
+
+import javax.persistence.*;
+import java.io.InputStream;
+import java.sql.Blob;
+
+@Entity
+@Table(name = "modules")
+public class Module {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "icon")
+    @Lob
+    private Blob icon;
+    @Column(name = "address")
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Blob getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Blob icon) {
+        this.icon = icon;
+    }
+}
