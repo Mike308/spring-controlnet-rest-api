3d9f3c600472aa032d8baf1884ddef88ed6f6ab8
Mikołaj Werner <m.werner9205@gmail.com>
2018-12-25 18:06:51 +0100

    - added method for get sensor count

--- src/main/java/com/controlnet/controlnetrestapi/service/SensorService.java
+++ src/main/java/com/controlnet/controlnetrestapi/service/SensorService.java
@@ -5,12 +5,34 @@ import com.controlnet.controlnetrestapi.repository.SensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class SensorService {
     @Autowired
     private SensorRepository sensorRepository;
 
+    private int getSizeOfIterable(Iterable<?> iterable){
+        if (iterable instanceof Collection)
+            return ((Collection<?>) iterable).size();
+        int i = 0;
+        for (Object object : iterable) i++;
+        return i;
+    }
+
     public Iterable<SensorView> getAllSensorByModuleId(int moduleId){
         return sensorRepository.getAllById(moduleId);
     }
+
+    public int getSensorCount(int moduleId){
+        return getSizeOfIterable(sensorRepository.getAllById(moduleId));
+    }
+
+
+
+
+
+
+
+
 }
