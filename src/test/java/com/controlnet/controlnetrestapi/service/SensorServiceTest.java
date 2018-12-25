eccfb51104f1c56b6e54657272247648e7d77873
Mikołaj Werner <m.werner9205@gmail.com>
2018-12-25 01:06:54 +0100

    - added unit test for sensor service

---
+++ src/test/java/com/controlnet/controlnetrestapi/service/SensorServiceTest.java
@@ -0,0 +1,35 @@
+package com.controlnet.controlnetrestapi.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SensorServiceTest {
+
+    @Autowired
+    private SensorService sensorService;
+
+
+    private int getSizeOfIterable(Iterable<?> iterable){
+        if (iterable instanceof Collection)
+            return ((Collection<?>) iterable).size();
+        int i = 0;
+        for (Object object : iterable) i++;
+        return i;
+    }
+
+    @Test
+    public void testGetAllSensorByModuleId(){
+        int size = getSizeOfIterable(sensorService.getAllSensorByModuleId(1));
+        assertTrue("FAILED: "+size, size > 0);
+    }
+
+}
