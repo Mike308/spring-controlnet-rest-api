560bd416d7213a84c9705f08ee1cc514a2711b98
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-03 21:49:07 +0200

    changed configuration to jwt handling

--- src/main/java/com/controlnet/controlnetrestapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/controlnet/controlnetrestapi/configuration/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package com.controlnet.controlnetrestapi.configuration;
 
 import com.controlnet.controlnetrestapi.component.CustomBasicAuthenticationEntryPoint;
 import com.controlnet.controlnetrestapi.repository.UserRepository;
+import com.controlnet.controlnetrestapi.security.JWTAuthentificationFilter;
+import com.controlnet.controlnetrestapi.security.JWTAuthorizationFilter;
 import com.controlnet.controlnetrestapi.service.CustomUserDetailsService;
+import com.controlnet.controlnetrestapi.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,20 +15,26 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-    @Autowired
-    private CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService).passwordEncoder(getPasswordEncoder());
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
 
     }
 
@@ -44,10 +54,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .anyRequest().permitAll()
+        http.cors().and().csrf().disable().
+        authorizeRequests()
+                .antMatchers("/hub/module/1").authenticated()
                 .and()
-                .httpBasic().authenticationEntryPoint(customBasicAuthenticationEntryPoint);
+                .addFilter(new JWTAuthentificationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
+
+
