628faa1646151acb2ef7e5a96230e2c3473f73a1
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-17 18:59:47 +0200

    added response entity

--- src/main/java/com/controlnet/controlnetrestapi/controller/SensorController.java
+++ src/main/java/com/controlnet/controlnetrestapi/controller/SensorController.java
@@ -5,6 +5,8 @@ import com.controlnet.controlnetrestapi.model.SensorSlotRequest;
 import com.controlnet.controlnetrestapi.model.SensorView;
 import com.controlnet.controlnetrestapi.service.SensorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,8 +21,9 @@ public class SensorController {
     }
 
     @PostMapping("/set-slot-name")
-    public boolean setSlotName(@RequestBody SensorSlotRequest sensorSlotRequest){
-        return sensorService.setSlotName(sensorSlotRequest.getOldName(), sensorSlotRequest.getNewName());
+    public ResponseEntity<SensorSlotRequest> setSlotName(@RequestBody SensorSlotRequest sensorSlotRequest){
+        boolean result = sensorService.setSlotName(sensorSlotRequest.getSensorId(), sensorSlotRequest.getNewName());
+        return new ResponseEntity<>(sensorSlotRequest, HttpStatus.OK);
     }
 
 
