24380ea611318eb9f26748b0b168138086a11db2
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-17 19:06:04 +0200

    added method for get sensor by sensor id

--- src/main/java/com/controlnet/controlnetrestapi/repository/SensorRepository.java
+++ src/main/java/com/controlnet/controlnetrestapi/repository/SensorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface SensorRepository extends CrudRepository <SensorView, Integer> {
     Iterable<SensorView> getAllById(int id);
-    SensorView getAllBySensorSlotName(String slotName);
+    SensorView getAllBySensorId(int sensorId);
 }
