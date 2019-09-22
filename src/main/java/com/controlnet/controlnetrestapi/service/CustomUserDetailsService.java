593fb9d43a7e523df0773981741a97d417a12592
Mikołaj Werner <m.werner9205@gmail.com>
2019-09-22 19:07:51 +0200

    code refactored

--- src/main/java/com/controlnet/controlnetrestapi/service/CustomUserDetailsService.java
+++ src/main/java/com/controlnet/controlnetrestapi/service/CustomUserDetailsService.java
@@ -4,10 +4,13 @@ import com.controlnet.controlnetrestapi.model.CustomUserDetails;
 import com.controlnet.controlnetrestapi.model.Users;
 import com.controlnet.controlnetrestapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -19,6 +22,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         Optional<Users> user = userRepository.findByName(userName);
         user.orElseThrow(() -> new UsernameNotFoundException("User not found!"));
-        return user.map(CustomUserDetails::new).get();
+        return new User(user.get().getName(), user.get().getPassword(), Collections.emptyList());
     }
 }
