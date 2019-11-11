af937b4a9fa9e64a37e3c8f26479fd6e715ee522
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-11 19:22:42 +0100

    added Temperature model (table temperatures)

---
+++ src/main/java/com/controlnet/controlnetrestapi/model/Temperature.java
@@ -0,0 +1,25 @@
+package com.controlnet.controlnetrestapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "temperatures")
+public class Temperature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "sensor_id")
+    private int sensorId;
+    @Column(name = "temperature")
+    private float temperature;
+    @Column(name = "date")
+    private String date;
+}
