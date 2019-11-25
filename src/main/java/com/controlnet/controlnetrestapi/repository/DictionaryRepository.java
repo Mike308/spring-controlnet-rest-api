1d2a480473e0bcd9d96d7006f00828e1da2df7c9
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-25 21:54:39 +0100

    added method for fetching only module types

--- src/main/java/com/controlnet/controlnetrestapi/repository/DictionaryRepository.java
+++ src/main/java/com/controlnet/controlnetrestapi/repository/DictionaryRepository.java
@@ -1,7 +1,13 @@
 package com.controlnet.controlnetrestapi.repository;
 
 import com.controlnet.controlnetrestapi.model.Dictionary;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DictionaryRepository extends CrudRepository <Dictionary, Integer> {
+    int countByValue(String value);
+    @Query(value = "select * from dictionary where val like '%_module'", nativeQuery = true)
+    List<Dictionary> getModuleTypes();
 }
