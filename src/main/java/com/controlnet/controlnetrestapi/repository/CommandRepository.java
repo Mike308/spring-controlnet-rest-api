f1158d591222529a88959b80abd6e572ae2fb5b3
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-25 21:54:01 +0100

    added command repository

---
+++ src/main/java/com/controlnet/controlnetrestapi/repository/CommandRepository.java
@@ -0,0 +1,10 @@
+package com.controlnet.controlnetrestapi.repository;
+
+import com.controlnet.controlnetrestapi.model.Command;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CommandRepository extends CrudRepository<Command, Integer> {
+    List<Command> getCommandByModuleIdOrderByCommandOrder(int moduleId);
+}
