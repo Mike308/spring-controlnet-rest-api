5be1ae4a32a3db6601b471b75c1da7d5eff2ab93
Mikołaj Werner <m.werner9205@gmail.com>
2019-12-21 23:36:47 +0100

    added method for delete all measurements by sensor id

--- src/main/java/com/controlnet/controlnetrestapi/repository/HumidityRepository.java
+++ src/main/java/com/controlnet/controlnetrestapi/repository/HumidityRepository.java
@@ -4,5 +4,5 @@ import com.controlnet.controlnetrestapi.model.Humidity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HumidityRepository extends CrudRepository <Humidity, Integer> {
-
+    void deleteBySensorId(int sensorId);
 }
