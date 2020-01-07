5f254fe485a57de4ab2bcd6fcb7752b794887d26
Mikołaj Werner <m.werner9205@gmail.com>
2020-01-07 19:05:08 +0100

    added method for fetching sensors by their codes

--- src/main/java/com/controlnet/controlnetrestapi/repository/SensorRepository.java
+++ src/main/java/com/controlnet/controlnetrestapi/repository/SensorRepository.java
@@ -4,8 +4,11 @@ import com.controlnet.controlnetrestapi.model.Sensor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SensorRepository extends CrudRepository <Sensor, Integer> {
     Sensor getSensorBySensorCode(String code);
     void deleteByModuleId(int moduleId);
+    List<Sensor> findAllBySensorCode(String sensorCode);
 }
