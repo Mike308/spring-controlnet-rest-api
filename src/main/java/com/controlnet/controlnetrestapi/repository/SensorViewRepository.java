9b0727dcdfa3ce1c698ec47e565d6ed2ab1d0035
Mikołaj Werner <m.werner9205@gmail.com>
2020-01-09 20:08:45 +0100

    replaced iterable on List

--- src/main/java/com/controlnet/controlnetrestapi/repository/SensorViewRepository.java
+++ src/main/java/com/controlnet/controlnetrestapi/repository/SensorViewRepository.java
@@ -3,7 +3,9 @@ package com.controlnet.controlnetrestapi.repository;
 import com.controlnet.controlnetrestapi.model.SensorView;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SensorViewRepository extends CrudRepository <SensorView, Integer> {
-    Iterable<SensorView> getAllById(int id);
+    List<SensorView> getAllById(int id);
     SensorView getAllBySensorId(int sensorId);
 }
