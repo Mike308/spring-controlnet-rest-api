2c5b04786186232f2be7f90c8c9861cc80859fb9
Mikołaj Werner <m.werner9205@gmail.com>
2020-01-27 20:07:16 +0100

    added password encoder

--- src/main/java/com/controlnet/controlnetrestapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/controlnet/controlnetrestapi/configuration/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,24 +28,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    private PasswordEncoder getPasswordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return charSequence.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s)  {
-                System.out.println("Char sequence: " + charSequence);
-                System.out.println("String: " + s);
-                return charSequence.equals(s);
-            }
-        };
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
