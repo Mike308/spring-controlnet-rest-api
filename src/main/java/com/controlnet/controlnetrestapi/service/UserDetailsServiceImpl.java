26d5722b5508bad6be9e8454cdbea2127bbdd205
Mikołaj Werner <m.werner9205@gmail.com>
2020-01-27 20:13:27 +0100

    added method for inserting admin user when no one admin user exist

--- src/main/java/com/controlnet/controlnetrestapi/service/UserDetailsServiceImpl.java
+++ src/main/java/com/controlnet/controlnetrestapi/service/UserDetailsServiceImpl.java
@@ -1,19 +1,23 @@
 package com.controlnet.controlnetrestapi.service;
 
-import com.controlnet.controlnetrestapi.model.CustomUserDetails;
-import com.controlnet.controlnetrestapi.model.Users;
+import com.controlnet.controlnetrestapi.model.User;
 import com.controlnet.controlnetrestapi.repository.UserRepository;
-import org.springframework.security.core.userdetails.User;
+import org.hibernate.exception.DataException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.Optional;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
+
 
     public UserDetailsServiceImpl(UserRepository userRepository){
         this.userRepository = userRepository;
@@ -21,8 +25,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        Optional<Users> user = userRepository.findByName(userName);
+        Optional<User> user = userRepository.findByName(userName);
         user.orElseThrow(() -> new UsernameNotFoundException("User not found!"));
-        return new User(user.get().getName(), user.get().getPassword(), Collections.emptyList());
+        return new org.springframework.security.core.userdetails.User(user.get().getName(), user.get().getPassword(), Collections.emptyList());
+    }
+
+    @PostConstruct
+    private void isUserAdminExisting() {
+        try {
+            loadUserByUsername("admin");
+        } catch (UsernameNotFoundException e) {
+            System.out.println("User admin not exists. Inserting....");
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+          User user = new User();
+          user.setName("admin");
+          user.setPassword(passwordEncoder.encode("xxxxx"));
+          try {
+              userRepository.save(user);
+          } catch (DataException ex) {
+              ex.printStackTrace();
+          }
+        }
     }
 }
