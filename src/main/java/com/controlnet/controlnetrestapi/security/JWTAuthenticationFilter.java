275aee650cfa075e4de0c32d2bbb3b0934cd130c
Mikołaj Werner <m.werner9205@gmail.com>
2020-02-10 21:33:59 +0100

    added improvements

--- src/main/java/com/controlnet/controlnetrestapi/security/JWTAuthenticationFilter.java
+++ src/main/java/com/controlnet/controlnetrestapi/security/JWTAuthenticationFilter.java
@@ -2,13 +2,11 @@ package com.controlnet.controlnetrestapi.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.controlnet.controlnetrestapi.model.Users;
+import com.controlnet.controlnetrestapi.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -21,21 +19,22 @@ import java.util.Date;
 
 import static com.controlnet.controlnetrestapi.security.SecurityConstants.*;
 
-public class JWTAuthentificationFilter extends UsernamePasswordAuthenticationFilter {
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
 
-    public JWTAuthentificationFilter(AuthenticationManager authenticationManager){
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager){
         this.authenticationManager = authenticationManager;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
         try{
-            System.out.println(httpServletRequest.getInputStream().toString());
-            Users users = new ObjectMapper().readValue(httpServletRequest.getInputStream(), Users.class);
-            return authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(users.getName(), users.getPassword(), new ArrayList<>()));
-        }catch (IOException e){
+            User user = new ObjectMapper().readValue(httpServletRequest.getInputStream(), User.class);
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword(), new ArrayList<>()));
+            return authentication;
+        }catch (Exception e){
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
@@ -44,12 +43,12 @@ public class JWTAuthentificationFilter extends UsernamePasswordAuthenticationFil
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult) throws IOException, ServletException {
         String token = JWT.create()
-                .withSubject(((User)authResult.getPrincipal()).getUsername())
+                .withSubject(((User)(authResult.getPrincipal())).getName())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .withClaim("userId",((User)(authResult.getPrincipal())).getId())
                 .sign(Algorithm.HMAC512(SECRET.getBytes()));
         response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
         response.setContentType("application/json");
         response.getWriter().write("{\"token\": " +"\""+ TOKEN_PREFIX + token +"\"" + "}");
-
     }
 }
